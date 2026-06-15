50dad176901b526ceefc8725c59a4a9c1e457ead
0x8bit144 <ramosmatthewoxq@hotmail.com>
2026-06-15 09:18:35 +0900

    Update forge-std to v1.14.0

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "openzeppelinjs/hardhats-upgrades";
 import "hardhat-contract-sizer";
 
 const config: HardhatUserConfig = {
